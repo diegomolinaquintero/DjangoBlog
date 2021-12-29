abe53e8f72024672965c88863b182ce6e02580f2
diegomolinaquintero <molinaquintero14@gmail.com>
2021-12-28 19:20:16 -0500

    sqlite3

--- easyblog/settings.py
+++ easyblog/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'easyblog.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default':  {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'girlsDjango',
-        'USER': 'girlsDjango',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default':  {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'girlsDjango',
+    #     'USER': 'girlsDjango',
+    #     'PASSWORD': '123',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
